0317eefb41028c97027348dde8785ad4008e2355
osasereweb <76796284+osasereweb@users.noreply.github.com>
2025-06-27 12:56:40 -0700

    Add files via upload

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import check_logo from '../images/check-verified.png';
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Payment() {
